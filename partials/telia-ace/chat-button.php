a7652180a94de1a094294fd73fb4e91bfb716f50
Billy Ward <billy@broomudigitals.fi>
2023-10-31 10:43:27 +0200

    feature/WY-590 add localization field to telia ace chat

--- partials/telia-ace/chat-button.php
+++ partials/telia-ace/chat-button.php
@@ -1,1 +1,1 @@
-<a class="telia-ace-chat-button" href="//acewebsdk/local-chat-info-fin"><?php echo $chat_name; ?></a>
+<a class="telia-ace-chat-button" href="//acewebsdk/<?php echo $localization; ?>"><?php echo $chat_name; ?></a>
