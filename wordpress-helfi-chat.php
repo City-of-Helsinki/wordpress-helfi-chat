3318257192fb5fb22cbdc7665f10ff93d863c2aa
Antti Koskinen <koskinenaa@users.noreply.github.com>
2025-12-03 11:38:39 +0200

    Update wordpress-helfi-chat.php

--- wordpress-helfi-chat.php
+++ wordpress-helfi-chat.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
   * Plugin Name: Helsinki Chat
   * Description: Adds chat integrations for various chat services.
@@ -16,56 +15,44 @@
 namespace CityOfHelsinki\WordPress\Chat;
 
 if ( ! defined( 'ABSPATH' ) ) {
-  exit;
+	exit;
 }
 
-function define_constants( string $file ) : void {
-	if ( ! function_exists('get_plugin_data') ) {
-		require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
-	}
-
-	$pluginData = \get_plugin_data( $file, false, false );
-	$dirname = dirname( $file );
-	$basename = basename( $file );
-	$dirbasename = basename( $dirname );
-
-	define( __NAMESPACE__ . '\\PLUGIN_VERSION', $pluginData[ 'Version' ] );
-	define( __NAMESPACE__ . '\\PLUGIN_MAIN_FILE', $file );
-	define( __NAMESPACE__ . '\\PLUGIN_PATH', $dirname . DIRECTORY_SEPARATOR );
-	define( __NAMESPACE__ . '\\PLUGIN_DIRNAME', $dirbasename );
-	define( __NAMESPACE__ . '\\PLUGIN_BASENAME', $basename );
-	define( __NAMESPACE__ . '\\PLUGIN_SLUG', str_replace( '-', '_', PLUGIN_DIRNAME ) );
-	define( __NAMESPACE__ . '\\PLUGIN_NAME', $dirbasename . DIRECTORY_SEPARATOR . $basename );
-	define( __NAMESPACE__ . '\\PLUGIN_URL', \plugin_dir_url( $file ) );
-}
-
+/**
+  * Setup
+  */
+require_once \plugin_dir_path( __FILE__ ) . 'constants.php';
 define_constants( __FILE__ );
 
-add_action( 'plugins_loaded', __NAMESPACE__ . '\\init', 100 );
-function init() {
+require_once \plugin_dir_path( __FILE__ ) . 'functions.php';
+load_includes();
 
+spl_autoload_register( __NAMESPACE__ . '\\class_loader' );
 
-	/**
-	  * Plugin parts
-	  */
-	require_once 'functions.php';
-	require_once 'settings/settings.php';
-	require_once 'integrations/wp-helfi-cookie-consent.php';
+\add_action( 'helsinki_chat_setup', __NAMESPACE__ . '\\setup_filters', 0 );
+\add_action( 'helsinki_chat_setup', __NAMESPACE__ . '\\load_features', 1 );
+\add_action( 'helsinki_chat_setup', __NAMESPACE__ . '\\load_integrations', 1 );
 
-	/**
-	 * Assets
-	 */
-	require_once 'class/assets.php';
+\add_action( 'plugins_loaded', __NAMESPACE__ . '\\setup', 1 );
+\add_action( 'plugins_loaded', __NAMESPACE__ . '\\loaded', 10 );
 
-	//spl_autoload_register( __NAMESPACE__ . '\\autoloader' );
+/**
+  * Init
+  */
+\add_action( 'init', __NAMESPACE__ . '\\textdomain' );
+\add_action( 'init', __NAMESPACE__ . '\\init', 100 );
 
-	/**
-	  * Actions & filters
-	  */
-	add_action( 'init', __NAMESPACE__ . '\\textdomain' );
+/**
+  * Activation
+  */
+// \register_activation_hook( __FILE__, __NAMESPACE__ . '\\activate' );
 
-	/**
-	  * Plugin ready
-	  */
-	do_action( 'helsinki_chat_init' );
-}
+/**
+  * Deactivation
+  */
+// \register_deactivation_hook( __FILE__, __NAMESPACE__ . '\\deactivate' );
+
+/**
+  * Uninstall
+  */
+// \register_uninstall_hook( __FILE__, __NAMESPACE__ . '\\uninstall' );
